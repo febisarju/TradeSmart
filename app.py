afb3b69a45b641f52c5ef44a4a0a18c440d929ad
Febi <146759828+febisarju@users.noreply.github.com>
2025-03-29 19:47:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 # Load trained model, scaler and label encoder
-model = joblib.load("random_forest_model.pkl")
-scaler = joblib.load("scaler.pkl")
-label_encoder = joblib.load("label_encoder.pkl") 
+model = joblib.load("model_files/random_forest_model.pkl")
+scaler = joblib.load("model_files/scaler.pkl")
+label_encoder = joblib.load("model_files/label_encoder.pkl") 
 
 app = Flask(__name__)
 
